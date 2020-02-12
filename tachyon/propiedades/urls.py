b1f6705661c029f026fd91e520cd9fbb742f632f
unknown <A01187388@itesm.mx>
2020-02-11 22:36:25 -0600

    Preparar FE de Propiedades

--- tachyon/propiedades/urls.py
+++ tachyon/propiedades/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('property', views.propertyView, name='porperty'),
+    path('properties', views.propertiesView, name='porperties'),
 ]
