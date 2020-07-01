1048b9ffd786f5a0b2a9e99aef5653c0b5b975be
abraham149 <A01207538@itesm.mx>
2020-07-01 16:08:14 -0500

    P08 - corregir issue en datatables

---
+++ tachyon/propiedades/static/propiedades/js/table-rev.js
@@ -0,0 +1,29 @@
+$(document).ready( function () {
+    $('#lista-propiedades-rev').DataTable( {
+    language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+        "infoEmpty": "Mostrando 0 a 0 de 0 entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar entradas _MENU_",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
+    "columnDefs": [
+      { "orderable": false, "targets": 5}
+    ]
+  });
+
+
+});
