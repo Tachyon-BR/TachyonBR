6a7aa4631c5c39c4bc503980e7c83887b0556e58
unknown <A01187388@itesm.mx>
2020-12-22 13:33:57 -0600

    Terminar P11

--- tachyon/root/static/root/js/random.js
+++ tachyon/root/static/root/js/random.js
@@ -19,5 +19,15 @@ function showProperty(property){
   $('#rp_price').html(property[0].fields.precio);
   $('#rp_address').html("" + property[0].fields.direccion + ", " + property[0].fields.estado + ", " + property[0].fields.codigo_postal);
   $('#rp_address').attr('href', "/propiedades/property/" + property[0].pk);
+	if(property[0].fields.tipo != "Terreno"){
+		$('#rp_banos').html(property[0].fields.banos);
+		$('#rp_habs').html(property[0].fields.habitaciones);
+		$('#rp_gar').html(property[0].fields.garaje);
+	}
+	else{
+		$('#rp_metros').html(property[0].fields.metros_terreno);
+		$('#rp_tipo1').prop('hidden', true);
+		$('#rp_tipo2').prop('hidden', false);
+	}
   $('#random_property').prop('hidden', false);
 }
